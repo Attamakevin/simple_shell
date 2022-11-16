e8c308f3d9d1142c91ad3d15442551f03765052e
attama <kevinejikez@gmail.com>
2022-11-16 10:42:53 -0800

    my first commit

---
+++ shell.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+/**
+* main - entry point to the program
+*
+* Return: return zero on success
+*/
+
+
+
+int main(void)
+
+{
+	char **argv = NULL;
+
+	my_getline();
+	my_exit(argv);
+return (0);
+}
