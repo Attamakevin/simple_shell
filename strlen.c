309d30bb1eff99fe163f4d7b77eaeddeb5a4c6c9
stan545 <kanuchigozie7@gmail.com>
2022-11-16 17:25:18 +0000

    Added function to get string length

---
+++ strlen.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+/**
+ * _strlen - function returns the length of a string
+ * @c: inputted string
+ * Return: len( The length of string)
+ */
+int _strlen(char *c)
+{
+	int len = 0;
+
+	while (*(c + i))
+	{
+		i++;
+	}
+	return (len);
+}
